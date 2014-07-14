d8f8e82a6d1be4f65e2b6a26930be4a32fcac3e4
rgarbi <richard.garbi@perceptivesoftware.com>
2014-07-14 21:03:04 +0100

    Use apt-get

--- install.sh
+++ install.sh
@@ -16,11 +16,11 @@ chef_binary=chef-solo
 if ! test -f "$chef_binary"; then
     export DEBIAN_FRONTEND=noninteractive
     # Upgrade headlessly (this is only safe-ish on vanilla systems)
-    aptitude update &&
+    apt-get update &&
     apt-get -o Dpkg::Options::="--force-confnew" \
         --force-yes -fuy dist-upgrade &&
     # Install Ruby and Chef
-    aptitude install -y ruby1.9.1 ruby1.9.1-dev make &&   
+    apt-get install -y ruby1.9.1 ruby1.9.1-dev make &&   
     sudo gem1.9.1 install --no-rdoc --no-ri chef --version 11.12.2
 fi &&
 
