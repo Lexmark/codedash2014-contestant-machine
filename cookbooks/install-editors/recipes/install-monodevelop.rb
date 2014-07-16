8cd42ec1f678ad96f23e5b2bf68eb38256583d30
rgarbi <richard.garbi@perceptivesoftware.com>
2014-07-16 16:51:02 +0100

    Install monodevelop using the ppa

--- cookbooks/install-editors/recipes/install-monodevelop.rb
+++ cookbooks/install-editors/recipes/install-monodevelop.rb
@@ -1,4 +1,11 @@
+bash "add mono develop apt repository" do  
+  code <<-EOF    
+    sudo add-apt-repository ppa:keks9n/monodevelop-latest -y
+    sudo apt-get update
+  EOF
+end
+
 #Install monodevelop
-package "monodevelop-current" do
+package "monodevelop-latest" do
   action :install
 end
