d0c5e5c406e334400400f492abe04514656d0b75
rgarbi <richard.garbi@perceptivesoftware.com>
2014-07-16 17:03:30 +0100

    Install monodevelop using a different ppa

--- cookbooks/install-editors/recipes/install-monodevelop.rb
+++ cookbooks/install-editors/recipes/install-monodevelop.rb
@@ -1,11 +1,11 @@
 bash "add mono develop apt repository" do  
   code <<-EOF    
-    sudo add-apt-repository ppa:keks9n/monodevelop-latest -y
+    sudo add-apt-repository ppa:ermshiperete/monodevelop -y
     sudo apt-get update
   EOF
 end
 
 #Install monodevelop
-package "monodevelop-latest" do
+package "monodevelop-current" do
   action :install
 end
