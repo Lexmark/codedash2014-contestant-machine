6831a8b5854d2f462640f755d6d2f8d2feff1e85
Richard <richard.garbi@perceptivesoftware.com>
2014-07-23 14:26:04 -0500

    Change a few of the create user commands

--- cookbooks/user-setup/recipes/default.rb
+++ cookbooks/user-setup/recipes/default.rb
@@ -1,7 +1,6 @@
 
-user "codedash" do
-  supports :manage_home => true
-  comment "Code Dash User"  
+user "codedash" do  
+  comment "codedash"  
   gid "root"
   home "/home/codedash"
   shell "/bin/bash"
