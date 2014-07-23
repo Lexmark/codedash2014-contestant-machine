ad35be16d6c26ce23a6f1ce4e222759a05c318a4
Richard <richard.garbi@perceptivesoftware.com>
2014-07-23 11:42:47 -0500

    Install vim and create user

---
+++ cookbooks/install-editors/recipes/install-launch-instructions.rb
@@ -0,0 +1,5 @@
+
+template 'Editor-Launch-Instructions.txt' do  
+  path "/home/codedash/Editor-Launch-Instructions.txt"  
+  source   'Editor-Launch-Instructions.txt'
+end
---
+++ cookbooks/install-editors/recipes/install-vim.rb
@@ -0,0 +1,9 @@
+#Install vim
+package "vim" do
+  action :install
+end
+
+#Install gvim
+package "vim-gtk" do
+  action :install
+end
