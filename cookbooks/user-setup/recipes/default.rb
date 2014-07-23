9bd871eed73136b09d2d64ad6d7fa562c0852306
Richard <richard.garbi@perceptivesoftware.com>
2014-07-23 15:36:08 -0500

    Make sure codedash owns its user home

--- cookbooks/user-setup/recipes/default.rb
+++ cookbooks/user-setup/recipes/default.rb
@@ -1,9 +1,15 @@
 
 user "codedash" do  
   comment "codedash"  
-  gid "root"
+  gid "users"
   home "/home/codedash"
   shell "/bin/bash"
   password "$1$22bgA1YD$JgBUYEoCwqv0lzYowJAtt/"
   action :create 
+end
+
+bash "change ownership of the " do  
+  code <<-EOF
+    chown -R codedash /home/codedash
+  EOF
 end
